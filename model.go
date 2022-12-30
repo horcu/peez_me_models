f8c5c84bbca5bd45cddbcef50a7b49ff86ccd3d8
Horatio Cummings <horatio.cummings@gmail.com>
2022-12-30 11:49:51 -0500

    changed room from string to struct

--- model.go
+++ model.go
@@ -82,7 +82,7 @@ type Ticket struct {
 	GameType         string       `json:"gameType"`
 	RoomType         string       `json:"roomType"`
 	IsActive         bool         `json:"isActive"`
-	Room             string       `json:"room"`
+	Room             Room         `json:"room"`
 	IsMatchTicket    bool         `json:"isMatchTicket"`
 	Created          string       `json:"created"`
 	Expires          string       `json:"expires"`
